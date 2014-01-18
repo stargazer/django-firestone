7d94093f84264cd94f9447d41a946c6c5320382b
stargazer <already.late@gmail.com>
2014-01-18 01:22:02 +0100

    Checking response's headers

--- gatorade/tests.py
+++ gatorade/tests.py
@@ -23,21 +23,23 @@ class BaseTest(TestCase):
         Runs through the response object, and verifies that it matches the
         expected response.
         """
-        response_status_code = response.status_code
-        response_content = json.loads(response.content)
-
         expected_status_code=kwargs.get('status_code')
+        expected_headers=kwargs.get('headers')
         expected_content=kwargs.get('content')
         expected_num_queries=kwargs.get('num_queries')
+        
+        response_status_code = response.status_code
+        response_content = json.loads(response.content)
+        response_headers = {header: response.get(header) for header in expected_headers}
 
         if expected_status_code:
             self.assertEqual(response_status_code, expected_status_code)
         if expected_content:
             self.assertEqual(type(response_content), type(expected_content))
             self.assertEqual(len(response_content), len(expected_content))
-        #if expected_headers:
-        #    for header, value in expected_headers.items():
-        #        self.assertTrue(response_headers[header], value)
+        if expected_headers:
+            for header, value in expected_headers.items():
+                self.assertTrue(response_headers[header], value)
         if expected_num_queries:
             # TODO: Check if there's a way to obtain the DB query number
             pass
