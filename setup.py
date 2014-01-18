cb0289805326ae353af06fc81e6a326015871e8d
stargazer <already.late@gmail.com>
2014-01-18 11:54:42 +0100

    retabbed setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-	name="django-gatorade",
-	packages=('gatorade', ),
-	version="0.1",
+    name="django-gatorade",
+    packages=('gatorade', ),
+    version="0.1",
     description="REST API Framework",
-	author="C. Paschalides",
-	author_email="already.late@gmail.com",
+    author="C. Paschalides",
+    author_email="already.late@gmail.com",
     license="WTFPL",
-	url="http://github.com/stargazer/django-gatorade",
+    url="http://github.com/stargazer/django-gatorade",
     keywords=("rest", "restful", "api", "crud"),
-	install_requires=(
-		"Django==1.5.4",
-	),
+    install_requires=(
+        "Django==1.5.4",
+    ),
     zip_safe=False,
     classifiers=(
         "Programming Language :: Python",
