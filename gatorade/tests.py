6bf97092f4f08f02adbb55dfe58678e625ef3bc8
stargazer <already.late@gmail.com>
2014-01-18 11:38:44 +0100

    Replaced the verify to the execute method, which does the same, but using a different interface.

--- gatorade/tests.py
+++ gatorade/tests.py
@@ -7,9 +7,17 @@ class BaseTest(TestCase):
     Very flexible testing class for testing API handlers.
 
     https://docs.djangoproject.com/en/1.5/topics/testing/overview/
+    https://docs.djangoproject.com/en/dev/ref/request-response/
     """
     def setUp(self):
         self.client = Client(HTTP_CONTENT_TYPE='application/json')
+        self.method_mapper = {
+            'get': self.client.get,
+            'post': self.client.post,
+            'put': self.client.put,
+            'delete': self.client.delete,
+        }                
+
         self.login()
 
     def login(self):
@@ -18,35 +26,57 @@ class BaseTest(TestCase):
             password=self.PASSWORD,
         )
 
-    def verify(self, response, **kwargs):
+    def execute(self, request, expected_response):
         """
-        Runs through the response object, and verifies that it matches the
-        expected response.
+        @param request: Request specs
+        @param response: Expected response specs
+
+        request = {
+            'method': 'get',
+            'path'  : '/api/handler/,
+            'data'  : {},
+            'headers': {},
+        }
+        expected_response = {
+            'status_code': 200,
+            'content_data_structure': list,
+            'content_num': 15,
+            'headers': {'content-disposition': 'attachment'},
+            ...
+            ...
+        }
         """
         # TODO: Be able to count num queries
         # TODO: Be able to compare the exact response with the expected
         # response
-        expected_response_data = {
-            'status_code': kwargs.get('status_code'),
-            'headers': kwargs.get('headers'),
-            'content_data_structure': kwargs.get('content_data_structure'),
-            'content_num': kwargs.get('content_num')
-            #'num_queries': kwargs.get('num_queries'),
-        }
+        
+        # Print request details
+        print 'Path:%s\tMethod:%s\tData:%s\tHeaders:%s\n' % (
+            request.get('path'),
+            request.get('method'),
+            request.get('data', {}),
+            request.get('headers', {}),
+        )
 
-        actual_response_data = {
+        # Execute request and retrieve response
+        response = self.method_mapper[
+            request['method']](path=request['path'], data=request['data'], **request['headers']
+        )
+        
+        # Extract the response details we need
+        response_data = {
             'status_code': response.status_code,
-            'headers': {header: response.get(header) for header in expected_response_data[headers]},
+            'headers': {header: response.get(header) for header in expected_response[headers]},
             'content_data_structure': type(response.content),
             'content_num': len(response.content),
-            #'num_queries':
         }                
-        
+
+        # Compare to expected
         for param in ('status_code', 'content_data_structure', 'content_num'):
-            self.assertEqual(actual_response_data(param), expected_response_data[param])
-        if expected_response_data.get('headers'):
-            for header, value in expected_response_data['headers'].items():
-                self.assertEqual(actual_response_data[header], value)
+            self.assertEqual(response_data(param), expected_response[param])
+        if expected_response.get('headers'):
+            for header, value in expected_response['headers'].items():
+                self.assertEqual(response_data[header], value)
 
 
 # Example of an application-level test class.
@@ -59,26 +89,32 @@ class HandlerTest(BaseTest):
         # This is an application level test method            
         def test_method_name(self):         
             # one request
-            response = self.client.get('/api/handler/')
-            self.verify(response,
-                status_code=200,
-                content_data_structure=list,
-                content_len=14,
-                headers={'content-disposition': 'attachment'},
-                num_queries=10,
-            )
+            request = {
+                'method': get,
+                'path': '/api/handler/'
+            }
+            response = {
+                'status_code': 200,
+                'content_data_structure': list,
+                'content_num': 14,
+                'headers': {'content-disposition': 'attachment'},
+                'num_queries': 10,
+            }
+            self.execute(request, response)
 
             # another request
-            response = self.client.post(
-                'v1/handler/',
-                json.dumps(dict(key=value)),
-                content_type='application/json',
-            )
-            self.verify(response,
-                status_code=200,
-                content_data_structure=dict,
-                content_len=10,
-                headers={'content-type': 'application/json'},
-                num_queries=2,
-            )
+            request = {
+                'method': 'post',
+                'path': '/api/handler/',
+                'data': {'key': 'value'},
+                'headers': {'content_type': 'application/json'},
+            }
+            response = {
+                'status_code': 200,
+                'content_data_structure': dict,
+                'content_num': 14,
+                'headers': {'content-type': 'application/json'},
+                'num_queries':2,
+            }
+            self.execute(request, response)
 
