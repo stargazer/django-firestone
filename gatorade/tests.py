b02d586117cf6bae81eb279a55c096d028bf58f6
stargazer <already.late@gmail.com>
2014-01-18 01:16:36 +0100

    Reminder to use fixtures for the example classes

--- gatorade/tests.py
+++ gatorade/tests.py
@@ -23,19 +23,21 @@ class BaseTest(TestCase):
         Runs through the response object, and verifies that it matches the
         expected response.
         """
-        response_content = response.content
-        response_headers = response.headers
+        response_status_code = response.status_code
+        response_content = json.loads(response.content)
 
+        expected_status_code=kwargs.get('status_code')
         expected_content=kwargs.get('content')
-        expected_headers=kwargs.get('headers')
         expected_num_queries=kwargs.get('num_queries')
 
+        if expected_status_code:
+            self.assertEqual(response_status_code, expected_status_code)
         if expected_content:
             self.assertEqual(type(response_content), type(expected_content))
             self.assertEqual(len(response_content), len(expected_content))
-        if expected_headers:
-            for header, value in expected_headers.items():
-                self.assertTrue(response_headers[header], value)
+        #if expected_headers:
+        #    for header, value in expected_headers.items():
+        #        self.assertTrue(response_headers[header], value)
         if expected_num_queries:
             # TODO: Check if there's a way to obtain the DB query number
             pass
@@ -46,11 +48,14 @@ class HandlerTest(BaseTest):
         USERNAME = 'tester@example.com'
         PASS     = 'pass'
 
+        fixtures = []
+
         # This is an application level test method            
         def test_method_name(self):         
             # one request
             response = self.client.get('/api/handler/')
             self.verify(response,
+                status_code=200,
                 content_type=list,
                 content_num=14,
                 headers={'content-disposition': 'attachment'},
@@ -64,6 +69,7 @@ class HandlerTest(BaseTest):
                 content_type='application/json',
             )
             self.verify(response,
+                status_code=200,
                 content_type=dict,
                 content_num=10,
                 headers={'content-type': 'application/json'},
