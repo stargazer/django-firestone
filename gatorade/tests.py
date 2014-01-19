15fa49a5644fa3dd33fac6c545918295d60d5e85
stargazer <already.late@gmail.com>
2014-01-19 11:34:12 +0100

    Abstracted away the request execution and response deserialization

--- gatorade/tests.py
+++ gatorade/tests.py
@@ -11,13 +11,6 @@ class BaseTest(TestCase):
     """
     def setUp(self):
         self.client = Client(HTTP_CONTENT_TYPE='application/json')
-        self.method_mapper = {
-            'GET': self.client.get,
-            'POST': self.client.post,
-            'PUT': self.client.put,
-            'DELETE': self.client.delete,
-        }                
-
         self.login()
 
     def login(self):
@@ -26,18 +19,64 @@ class BaseTest(TestCase):
             password=self.PASSWORD,
         )
 
-    def execute(self, request, expected_response):
+    def request(self, request_dict):
         """
-        @param request: Request specs
-        @param response: Expected response specs
+        @param request_dict: Dictionary with request specs
 
-        request = {
+        Deserializes the ``request_dict``, translate it to an HTTP request
+        on the ``self.client`` client, executres the request, and returns the
+        HttpResponse object.
+        """
+        method_mapper = {
+            'GET': self.client.get,
+            'POST': self.client.post,
+            'PUT': self.client.put,
+            'DELETE': self.client.delete,
+        }                
+
+        method = request.get('method').upper()
+        path = request.get('path')
+        data = request.get('data', {})
+        headers = request.get('headers', {})
+
+        # Print request details
+        print 'Path:%s\tMethod:%s\tData:%s\tHeaders:%s\n' % (
+            path,
+            method,
+            data,
+            headers,
+        )
+
+        # Execute request and return response object
+        return method_mapper[method](path=path, data=data, **headers)
+
+    def response(self, response, expected_response_dict):
+        """
+        @param response: HTTPResponse object
+        @param expected_response_dict: Dictionary with expected response specs
+
+        Takes the ``response`` object and returns a dictionary with its values
+        we are interested in
+        """
+        return {
+            'status_code': response.status_code,
+            'headers': {header: response.get(header) for header in expected_response_dict['headers']},
+            'data': type(response.content),
+            'len': len(response.content),
+        }
+
+    def execute(self, request_dict, expected_response_dict):
+        """
+        @param request_dict: Dictionary with request specs
+        @param expected_response_dict: Dictionary with expected response specs
+
+        request_dict = {
             'method': 'get',
             'path'  : '/api/handler/,
             'data'  : {},
             'headers': {},
         }
-        expected_response = {
+        expected_response_dict = {
             'status_code': 200,
             'data': list,
             'len': 15,
@@ -48,34 +87,17 @@ class BaseTest(TestCase):
         # TODO: Be able to compare the exact response with the expected
         # response
         
-        # Print request details
-        print 'Path:%s\tMethod:%s\tData:%s\tHeaders:%s\n' % (
-            request.get('path'),
-            request.get('method'),
-            request.get('data', {}),
-            request.get('headers', {}),
-        )
-
         # Execute request and retrieve response
-        response = self.method_mapper[
-            request['method'.upper()]](path=request['path'], data=request['data'], **request['headers']
-        )
-        
+        response = self.request(request_dict)
         # Extract the response details we need
-        response_data = {
-            'status_code': response.status_code,
-            'headers': {header: response.get(header) for header in expected_response[headers]},
-            'data': type(response.content),
-            'len': len(response.content),
-        }                
-
+        actual_response_dict = self.response(response, expected_response_dict)
+        
         # Compare to expected
         for param in ('status_code', 'data', 'len'):
-            self.assertEqual(response_data(param), expected_response[param])
+            self.assertEqual(actual_response_dict(param), expected_response_dict[param])
         if expected_response.get('headers'):
-            for header, value in expected_response['headers'].items():
-                self.assertEqual(response_data[header], value)
-
+            for header, value in expected_response_dict['headers'].items():
+                self.assertEqual(actual_response_dict[header], value)
 
 # Example of an application-level test class.
 class HandlerTest(BaseTest):
