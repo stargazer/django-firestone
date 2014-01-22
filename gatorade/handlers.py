c4fa8be91f573ea3f9ae128db6951d001f76ab5b
stargazer <already.late@gmail.com>
2014-01-22 14:08:16 +0100

    Very simple outline of BaseHandler and ModelHandler. Output field definition through the *template* parameter. Serialization to python data structures, is done by method *serialize_to_python*

---
+++ gatorade/handlers.py
@@ -0,0 +1,59 @@
+class BaseHandler(object):
+    # Override to define the handler's output representation. It should follow
+    # the syntax of ``django-preserialize`` templates.
+    # See <https://github.com/bruth/django-preserialize#conventions>
+    template = {}           
+
+    def serialize_to_python(self, data):
+        """
+        Serializes the output of a handler's operation (which is always a model
+        or queryset) to python data structures, according to the definition of
+        the handler's ``template`` variable.
+        """
+        return serialize(data, **self.template)
+
+class ModelHandler(object):
+    # Override to define the handler's model
+    model = None
+
+### Example of BaseHandler
+class RandomHandler(BaseHandler):
+    user_template = {
+        'fields': ['id', 'domain', 'email'],
+    }
+    push_template = {
+        'fields': ['id', 'creator'],
+        'related': {
+            'creator': user_template
+        },
+    }
+    nouncy_template = {
+        'fields': ['id', 'title', 'pushes', 'user'],
+        'related': {
+            'pushes': push_template,
+            'user'  : user_template,
+        },
+    }
+    # Say this handler outputs a dictionary like 
+    # {'a': <value>, 'b': <value>, 'c': <Nouncy>}
+    template = {
+        'fields': ['a', 'b', 'nouncy'],
+        'related' = {
+            'nouncy': nouncy_template,
+        }
+    }            
+
+### Example of ModelHandler
+class ContactHandler(ModelHandler):
+    model = Contact
+
+    media_template = {
+        'exclude' = ['user',],
+    }
+    template = {
+        'exclude' = ['user',],
+        'related' = {
+            'media': media_template'
+        },
+    }
+
