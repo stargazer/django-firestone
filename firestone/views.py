831c7ecfe9f96203c502e341efabb65b69712dba
stargazer <already.late@gmail.com>
2014-01-28 17:07:11 +0200

    view.View mimics the behavior of Django's generic CBVs

--- firestone/views.py
+++ firestone/views.py
@@ -1,6 +1,4 @@
-
-
-class View():
+class View(object):
     def __init__(self, args):
         if not isinstance(args, list):
             args = args,
@@ -9,6 +7,12 @@ class View():
     def __call__(self, request, *args, **kwargs):
         for handler in self.handlers:
             h = handler()
+
+            # Mimics the behavior of Django's class based views ``as_view`` method
+            h.request = request
+            h.args = args
+            h.kwargs = kwargs
+
             if h.authentication.is_authenticated(request, *args, **kwargs):
                 return h.execute(request, *args, **kwargs)
 
