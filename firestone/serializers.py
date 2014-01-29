0c108da1a08a2d319b7dce85e95e7d4126695390
stargazer <already.late@gmail.com>
2014-01-29 17:56:48 +0200

    First iteration of serializer

--- firestone/serializers.py
+++ firestone/serializers.py
@@ -2,9 +2,26 @@
 # IF supported, output to that format. If not supported, fallback to default
 # serializer. Along with the serialized response, return the response's Content-type
 
-def serialize_to_json():
+import json
+
+def _serialize_to_json(data):
     """
-    Takes a python data structure, and outputs its json representation
+    Serializes ``data`` into JSON, and returns a tuple
+       serialized data, HttpResponse headers as dictionary key-values
     """
-    pass
+    return json.dumps(data), {'content_type': 'application/json'}
+
+DEFAULT_SERIALIZER = _serialize_to_json
+def _get_serializer(request, *args, **kwargs):
+    """
+    Returns the function that will serialize the data
+    """
+    return DEFAULT_SERIALIZER
+
+def serialize(data, request, *args, **kwargs):
+    """
+    Selects the appropriate serializer, calls it, and returns its result
+    """
+    s = _get_serializer(request, *args, **kwargs)
+    return s(data)
 
---
+++ testproject/users/tests/test_serializers.py
@@ -0,0 +1,58 @@
+"""
+This module tests the functions in the ``firestone.serializers`` module
+"""
+from firestone import serializers
+from firestone.handlers import BaseHandler
+from firestone.handlers import ModelHandler
+from django.test import TestCase
+from django.test import RequestFactory
+import json
+
+
+class TestSerializers(TestCase):
+    def test_serialize_to_json(self):
+        data = 'somedata'
+
+        result, headers = serializers._serialize_to_json(data) 
+        self.assertJSONEqual(result, json.dumps(data))
+        self.assertEquals(headers, {'content_type': 'application/json'})
+
+        data = [1, 2, 3]
+        result, headers = serializers._serialize_to_json(data)
+        self.assertJSONEqual(result, json.dumps(data))
+        self.assertEquals(headers, {'content_type': 'application/json'})
+
+        data = {'key': 'value'}
+        result, headers = serializers._serialize_to_json(data)
+        self.assertJSONEqual(result, json.dumps(data))
+        self.assertEquals(headers, {'content_type': 'application/json'})
+
+    def test_get_serializer(self):
+        request = RequestFactory().get('/')
+                
+        self.assertEquals(
+            serializers._get_serializer(request),
+            serializers._serialize_to_json
+        )
+
+    def test_serialize(self):
+        request = RequestFactory().get('/')
+        
+        data = 'somedata'
+        result, headers = serializers.serialize(data, request) 
+        self.assertJSONEqual(result, json.dumps(data))
+        self.assertEquals(headers, {'content_type': 'application/json'})
+
+        data = [1, 2, 3]
+        result, headers = serializers.serialize(data, request)
+        self.assertJSONEqual(result, json.dumps(data))
+        self.assertEquals(headers, {'content_type': 'application/json'})
+
+        data = {'key': 'value'}
+        result, headers = serializers.serialize(data, request)
+        self.assertJSONEqual(result, json.dumps(data))
+        self.assertEquals(headers, {'content_type': 'application/json'})
+
+
+
+
