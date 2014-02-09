03e89fdb68c1ab2b7977146279b19b06bbb13f7b
stargazer <already.late@gmail.com>
2014-02-09 22:20:56 +0200

    More tests for the ModelHandler.clean_models method

--- testproject/testapp/tests/test_handlers_clean_models.py
+++ testproject/testapp/tests/test_handlers_clean_models.py
@@ -52,6 +52,8 @@ class TestCleanModelsSingleModel(TestCase):
         except exceptions.BadRequest, e:
             self.assertIsInstance(e.errors, dict)
             self.assertItemsEqual(e.errors.keys(), ('username', 'password'))
+        else:
+            assert(False)
 
     def test_invalid_general(self):
         """
@@ -84,6 +86,8 @@ class TestCleanModelsSingleModel(TestCase):
         except exceptions.BadRequest, e:
             self.assertIsInstance(e.errors, dict)
             self.assertEqual(e.errors['__all__'][0], 'Error string')
+        else:
+            assert(False)
 
         User.clean = old_clean            
 
@@ -92,15 +96,84 @@ class TestCleanModelsQueryset(TestCase):
     Errors on some model instance in a queryset, will behave exactly as in the
     case of a single model instance.
     """
-
     def setUp(self):
-       mommy.make(User, 10)
+        mommy.make(User, 10)
 
     def test_correct(self):
-        pass
+        handler = ModelHandler()
+        handler.model = User
+
+        request = RequestFactory().post('/')
+        request.data = User.objects.all()
+
+        handler.clean_models(request)
 
     def test_invalid_field_values(self):
-        pass
+        """
+        We will set some invalid field values.
+
+        Are errors raised by model.clean_fields() handled correctly?
+        They should raise a ``exceptions.BadRequest`` exception
+        """
+        handler = ModelHandler()
+        handler.model = User
+        
+        request = RequestFactory().post('/')
+        request.data = User.objects.all()
+
+        # I set some invalid values for some fields of every instance
+        for item in request.data:
+            item.username = ''
+            item.password = ''
+
+        # Does ``clean_models`` raise the correct exception?
+        self.assertRaises(
+            exceptions.BadRequest,
+            handler.clean_models,
+            request,
+        )
+
+        # Does the exception ``errors`` attribute include a dictionary with the correct keys?
+        try:
+            handler.clean_models(request)
+        except exceptions.BadRequest, e:
+            self.assertIsInstance(e.errors, dict)
+            self.assertItemsEqual(e.errors.keys(), ('username', 'password'))
+        else:
+            assert(False)
     
     def test_invalid_general(self):
-        pass
+        """
+        We will make the model's clean() method to raise some ValidationError.
+
+        Are errors raised by model.clean() handler correctly?
+        They should raise a ``exceptions.BadRequest`` exception.
+        """
+        old_clean = User.clean
+        def clean(self):
+            raise ValidationError('Error string')
+        User.clean = clean
+
+        handler = ModelHandler()
+        handler.model = User
+        
+        request = RequestFactory().post('/')
+        request.data = User.objects.all()
+        
+        # Does ``clean_models`` raise the correct exception?
+        self.assertRaises(
+            exceptions.BadRequest,
+            handler.clean_models,
+            request,
+        )
+        # Does the exception ``errors`` attribute include a dictionary of the
+        # correct form?
+        try:
+            handler.clean_models(request)
+        except exceptions.BadRequest, e:
+            self.assertIsInstance(e.errors, dict)
+            self.assertEqual(e.errors['__all__'][0], 'Error string')
+        else:
+            assert(False)
+
+        User.clean = old_clean            
