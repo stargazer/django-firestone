1429ca44c5f00707d8283e23af97edcd39a1b5a5
stargazer <already.late@gmail.com>
2014-07-15 12:18:20 +0300

    And the corresponding tests for method is_catastrophic()

--- testproject/testapp/tests/__init__.py
+++ testproject/testapp/tests/__init__.py
@@ -18,6 +18,7 @@ from test_handlers_cleanse_body import *
 from test_handlers_validate import *
 from test_handlers_clean_models import *
 from test_handlers_get import *
+from test_handlers_is_catastrophic import *
 from test_handlers_post import *
 from test_handlers_put import *
 from test_handlers_delete import *
