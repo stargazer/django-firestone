f96fec40d155c2381f653dce0b891634d825b1ef
stargazer <already.late@gmail.com>
2014-07-16 16:28:46 +0300

    Expanding the testproject.testapp with another model

---
+++ testproject/testapp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+
+class Contact(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(unique=True, blank=False)
+
+    def clean_fields(self, exclude=None):
+        try:
+            validate_email(self.email)
+        except ValidationError:
+            raise
+
+        super(Contact, self).clean_fields(exclude)
+
+admin.site.register(Contact)        
