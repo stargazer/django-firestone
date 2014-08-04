1a13239b32de0b92158a87d02941ba2c10ff4405
stargazer <already.late@gmail.com>
2014-08-04 22:49:16 +0300

    Future Django versions deal differently with the app to test, so I account for them here

--- runtests.py
+++ runtests.py
@@ -5,10 +5,14 @@ sys.path.insert(0, test_dir)
 
 from django.test.utils import get_runner
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 def runtests():
     TestRunner = get_runner(settings)
     test_runner = TestRunner(verbosity=1, interactive=True)
-    failures = test_runner.run_tests(['testapp',])
+    try:
+        failures = test_runner.run_tests(['testapp',])
+    except ImportError:
+        failures = test_runner.run_tests(['testproject.testapp',])
     sys.exit(failures)
 
