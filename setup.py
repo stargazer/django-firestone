546f15734a863a5d54f1d6610654c0b7d9ac97b3
stargazer <already.late@gmail.com>
2014-08-05 10:29:49 +0300

    Version 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='django-firestone',
     packages=('firestone', ),
-    version='0.1.1',
+    version='0.2',
     description='REST API Framework',
     author='C. Paschalides',
     author_email='already.late@gmail.com',
