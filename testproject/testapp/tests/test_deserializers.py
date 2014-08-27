e01f3727f96b039bb8a6715c70f423fc915729e3
stargazer <already.late@gmail.com>
2014-08-27 14:01:08 +0300

    Refactored test module, and added tests for *application/x-www-form-urlencoded* deserializer

--- testproject/testapp/tests/test_deserializers.py
+++ testproject/testapp/tests/test_deserializers.py
@@ -1,50 +1,82 @@
 """
 This module tests the ``firestone.deserializers`` module
 """
-from django.test import TestCase
-from django.test import RequestFactory
 from firestone.deserializers import _json_deserializer
+from firestone.deserializers import _form_encoded_data_deserializer
 from firestone.deserializers import _get_deserializer
 from firestone.deserializers import deserialize
 from firestone.deserializers import deserialize_request_body
 from firestone import exceptions
+from django.test import TestCase
+from django.test import RequestFactory
+import urllib
 
 
-class TestDeserializers(TestCase):
-    def test_json_deserializer(self):
+class TestJsonDeserializer(TestCase):
+    def test_string(self):
         data = 'string'
         self.assertRaises(ValueError, _json_deserializer, data)
 
+    def test_list(self):
         data = '[1, 2, 3]'
         expected = [1, 2, 3]
         self.assertEqual(_json_deserializer(data), expected)
 
+    def test_json(self):
         data = '{"key": "value"}'
         expected = {'key': 'value'}
         self.assertEqual(_json_deserializer(data), expected)
 
+class TestFormEncodedDataDeserializer(TestCase):
+    def test_string(self):
+        data = 'string'
+        self.assertRaises(ValueError, _form_encoded_data_deserializer, data)
 
-    def test_get_deserializer(self):
+    def test_urlencoded(self):
+        data_dic = {'key1': 'value1', 'key2': 'value2'}
+        data = urllib.urlencode(data_dic)
+        self.assertEqual(_form_encoded_data_deserializer(data), data_dic)
+
+class TestGetDeserializer(TestCase):
+    def test_empty(self):
         self.assertIsNone(_get_deserializer(''))
 
+    def test_unsupported(self):        
         self.assertIsNone(_get_deserializer('unsupported media type'))
         
+    def test_json(self):        
         self.assertEquals(
             _get_deserializer('application/json'),
             _json_deserializer,
         )
 
+    def test_JSON(self):        
         self.assertEquals(
             _get_deserializer('Application/JSON'),
             _json_deserializer,
         )
 
+    def test_json_utf8(self):        
         self.assertEquals(
             _get_deserializer('application/json; charset=utf-8'),
             _json_deserializer,
         )
 
-    def test_deserialize(self):
+    def test_form_encoded(self):        
+        self.assertEquals(
+            _get_deserializer('application/x-www-form-urlencoded'),
+            _form_encoded_data_deserializer,
+        )                
+
+    def test_FORM_ENCODED(self):        
+        self.assertEquals(
+            _get_deserializer('application/X-WWW-FORM-URLENCODED'),
+            _form_encoded_data_deserializer,
+        )                
+
+
+class TestDeserialize(TestCase):        
+    def test_json_none_content_type(self):
         data = '{"key": "value"}'
         # Empty ``content_type``
         self.assertRaises(
@@ -52,12 +84,16 @@ class TestDeserializers(TestCase):
             deserialize,
             data, None
         )
+    def test_json_empty_content_type(self):        
+        data = '{"key": "value"}'
         self.assertRaises(
             exceptions.UnsupportedMediaType,
             deserialize,
             data, ''
         )
-
+    
+    def test_json_unsupported_content_type(self):        
+        data = '{"key": "value"}'
         # Unsupported ``content_type``
         self.assertRaises(
             exceptions.UnsupportedMediaType,
@@ -65,6 +101,7 @@ class TestDeserializers(TestCase):
             data, 'unsupported_content_type'
         )
 
+    def test_invalid_data_json_content_type(self):        
         # Known content_type, bad data
         data = "string"
         self.assertRaises(
@@ -72,6 +109,7 @@ class TestDeserializers(TestCase):
             deserialize,
             data, 'application/json',
         )
+
         data = "{key: value}"
         self.assertRaises(
             exceptions.BadRequest,
@@ -79,39 +117,55 @@ class TestDeserializers(TestCase):
             data, 'application/json',
         )
 
+    def test_invalid_data_form_content_type(self):        
+        data = "string"
+        self.assertRaises(
+            exceptions.BadRequest,
+            deserialize,
+            data, 'application/x-www-form-urlencoded',
+        )
 
+    def test_valid_data_json_content_type(self):            
         # Known content_type, good data
         data = '{"key": "value"}'
         self.assertEquals(
             deserialize(data, 'application/json'),
             {'key': 'value'},
         )
+
+    def test_valid_data_JSON_content_type(self):
+        data = '{"key": "value"}'
         self.assertEquals(
             deserialize(data, 'Application/JSON'),
             {'key': 'value'},
         )
+
+    def test_valid_data_json_utf_content_type(self): 
+        data = '{"key": "value"}'
         self.assertEquals(
             deserialize(data, 'application/json; charset=utf-8'),
             {'key': 'value'},
+        )
+
+    def test_valid_data_form_content_type(self):            
+        data_dic = {'key': 'value'}
+        data = urllib.urlencode(data_dic)
+        self.assertEquals(
+            deserialize(data, 'application/x-www-form-urlencoded'),
+            data_dic,
+        )
+
+    def test_valid_data_FORM_content_type(self):        
+        data_dic = {'key': 'value'}
+        data = urllib.urlencode(data_dic)
+        self.assertEquals(
+            deserialize(data, 'application/X-WWW-FORM-URLENCODED'),
+            data_dic
         )
         
-        # Known content_type, good data
-        data = "[1, 2, 3]"
-        self.assertEquals(
-            deserialize(data, 'application/json'),
-            [1, 2, 3],
-        )
-        self.assertEquals(
-            deserialize(data, 'Application/JSON'),
-            [1, 2, 3],
-        )
-        self.assertEquals(
-            deserialize(data, 'application/json; charset=utf-8'),
-            [1, 2, 3],
-        )
 
-
-    def test_get_deserialize_request_body_post(self):
+class TestGetDeserializeRequestBody(TestCase):        
+    def test_empty_content_type(self):
         """ POST requests"""
         # Empty ``Content-type`` header
         request = RequestFactory().post(
@@ -124,7 +178,8 @@ class TestDeserializers(TestCase):
             deserialize_request_body,
             request,
         )
-        
+
+    def test_unsupported_media_type(self):        
         # Unsupported media type
         request = RequestFactory().post(
             '/', 
@@ -137,6 +192,7 @@ class TestDeserializers(TestCase):
             request,
         )
 
+    def test_json_content_type_invalid_data_1(self):        
         # Known Content-Type, bad data
         request = RequestFactory().post(
             '/', 
@@ -149,6 +205,7 @@ class TestDeserializers(TestCase):
             request,
         )
 
+    def test_json_content_type_invalid_data_2(self):        
         # Known Content-Type, bad data
         request = RequestFactory().post(
             '/', 
@@ -161,6 +218,19 @@ class TestDeserializers(TestCase):
             request,
         )
 
+    def test_form_content_type_invalid_data(self):
+        request = RequestFactory().post(
+            '/',
+            data='string',
+            content_type='application/x-www-form-urlencoded',
+        )
+        self.assertRaises(
+            exceptions.BadRequest,
+            deserialize_request_body,
+            request,
+        )
+
+    def test_json_content_type_valid_data_1(self):        
         # Known Content-Type, good data
         request = RequestFactory().post(
             '/', 
@@ -172,6 +242,7 @@ class TestDeserializers(TestCase):
             [1, 2, 3],
         )
 
+    def test_json_content_type_valid_data_2(self):        
         # Known Content-Type, good data
         request = RequestFactory().post(
             '/', 
@@ -181,80 +252,20 @@ class TestDeserializers(TestCase):
         self.assertEqual(
             deserialize_request_body(request),
             {'key': 'value'},
+        )
+
+    def test_form_content_type_valid_data(self):
+        data_dic = {'key1': 'value1', 'key2': 'value2'}
+
+        request = RequestFactory().post(
+            '/',
+            data=urllib.urlencode(data_dic),
+            content_type='application/x-www-form-urlencoded',
+        )
+        self.assertEqual(
+            deserialize_request_body(request),
+            data_dic,
         )
 
     
-    def test_get_deserialize_request_body_put(self):
-        """ PUT requests"""
 
-        # Empty ``Content-type`` header
-        request = RequestFactory().put(
-            '/', 
-            data='{"key": "value"}',
-            content_type='',
-        )
-        self.assertRaises(
-            exceptions.UnsupportedMediaType,
-            deserialize_request_body,
-            request,
-        )
-        
-        # Unsupported media type
-        request = RequestFactory().put(
-            '/', 
-            data='{"key": "value"}',
-            content_type='unsupported media type',
-        )
-        self.assertRaises(
-            exceptions.UnsupportedMediaType,
-            deserialize_request_body,
-            request,
-        )
-
-        # Known Content-Type, bad data
-        request = RequestFactory().put(
-            '/', 
-            data='string',
-            content_type='application/json',
-        )
-        self.assertRaises(
-            exceptions.BadRequest,
-            deserialize_request_body,
-            request,
-        )
-
-        # Known Content-Type, bad data
-        request = RequestFactory().put(
-            '/', 
-            data='{lala}',
-            content_type='application/json',
-        )
-        self.assertRaises(
-            exceptions.BadRequest,
-            deserialize_request_body,
-            request,
-        )
-
-        # Known Content-Type, good data
-        request = RequestFactory().put(
-            '/', 
-            data='[1, 2, 3]',
-            content_type='application/json',
-        )
-        self.assertEqual(
-            deserialize_request_body(request),
-            [1, 2, 3],
-        )
-
-        # Known Content-Type, good data
-        request = RequestFactory().put(
-            '/', 
-            data='{"key": "value"}',
-            content_type='application/json',
-        )
-        self.assertEqual(
-            deserialize_request_body(request),
-            {'key': 'value'},
-        )
-
- 
