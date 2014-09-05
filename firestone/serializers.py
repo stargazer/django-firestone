0864af36f712caa87c6bbceae4c50ac4048d9298
stargazer <already.late@gmail.com>
2014-09-05 11:12:20 +0300

    All these functions are now obsolete

--- firestone/serializers.py
+++ firestone/serializers.py
@@ -2,44 +2,6 @@ from django.http import HttpResponse
 from django.core.serializers.json import DateTimeAwareJSONEncoder
 import json
 
-def _serialize_to_json(data):
-    """
-    Serializes ``data`` into JSON, and returns a tuple
-       serialized data, HttpResponse headers as dictionary key-values
-    """
-    return json.dumps(data, cls=DateTimeAwareJSONEncoder, indent=4), {'content-type': 'application/json'}
-
-def _serialize_to_excel(data):
-    return '', {
-        'content-type': 'application/vnd.ms-excel',
-        'content-disposition': 'attachment'
-    } 
-
-DEFAULT_SERIALIZATION_FORMAT = 'application/json'
-MAPPER = {
-    'application/json': _serialize_to_json,
-    'application/vnd.ms-excel': _serialize_to_excel,
-}        
-def _get_serializer(ser_format=DEFAULT_SERIALIZATION_FORMAT):
-    """
-    Returns the serialization function
-    """
-    return MAPPER.get(ser_format, MAPPER[DEFAULT_SERIALIZATION_FORMAT])
-
-def _get_serialization_format(request):
-    """
-    Returns the serialization format that the ``request``'s ``Accept`` header
-    wants. Returns ``DEFAULT_SERIALIZATION_FORMAT`` if header doesn't match any
-    of the provided serializaion formats.
-    """
-    accept_header = request.META.get('HTTP_ACCEPT', '')
-    if accept_header:
-        accept_header = accept_header.split(',')
-        accept_header = [value.strip().lower() for value in accept_header]
-        for preference in accept_header:
-            if preference in MAPPER:
-                return preference
-    return DEFAULT_SERIALIZATION_FORMAT
 
 def serialize(data, ser_format=DEFAULT_SERIALIZATION_FORMAT):
     """
@@ -59,7 +21,19 @@ class SerializerMixin(object):
     }
 
     def get_serialization_format(self):
-        return _get_serialization_format(self.request)
+        """
+        Returns the serialization format that the ``request``'s ``Accept`` header
+        wants. Returns ``DEFAULT_SERIALIZATION_FORMAT`` if header doesn't match any
+        of the provided serializaion formats.
+        """
+        accept_header = self.request.META.get('HTTP_ACCEPT', '')
+        if accept_header:
+            accept_header = accept_header.split(',')
+            accept_header = [value.strip().lower() for value in accept_header]
+            for preference in accept_header:
+                if preference in self.MAPPER:
+                    return preference
+        return self.DEFAULT_SERIALIZATION_FORMAT
 
     def get_serializer(self, ser_format):
         return getattr(self, self.MAPPER[ser_format]) 
