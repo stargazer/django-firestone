80658198b4371d76999c8af9f65b3d7e988f70c5
stargazer <already.late@gmail.com>
2014-09-06 21:14:41 +0300

    Testing a model handler's whole data flow

--- testproject/testapp/tests/test_whole_flow.py
+++ testproject/testapp/tests/test_whole_flow.py
@@ -5,12 +5,17 @@ from firestone.proxy import Proxy
 from firestone.handlers import BaseHandler
 from firestone.handlers import ModelHandler
 from firestone.authentication import SessionAuthentication
+from firestone.authentication import JWTAuthentication
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test import RequestFactory
+from django.utils import timezone
+from django.conf import settings
 from model_mommy import mommy
 from random import randint
+from datetime import timedelta
 import json
+import jwt
 
 
 class BaseHandlerExample(BaseHandler):
@@ -103,4 +108,211 @@ class TestBaseHandler(TestCase):
         response = basehandler_proxy(request)
         self.assertEqual(response.status_code, 405)
 
+
+
+class ModelHandlerExample(ModelHandler):
+    authentication = JWTAuthentication
+    model = User
+    http_methods = ['GET', 'POST', 'DELETE']
+    post_body_fields = ['first_name', 'last_name', 'username', 'password']
+    filters = ('filter_id',)
+
+    content_type_template = {
+        'fields': ['id', ],
+        'flat': False,
+    }            
+    logentry_template = {
+        'fields': ['action_flag', 'content_type'],     
+        'related': {
+            'content_type': content_type_template,
+        }
+    }
+    template = {
+        'fields': ['id', 'username', 'first_name', 'last_name',
+                   'logentry_set', 'email', 'last_login', 'nickname'], 
+        'related': {
+            'logentry_set': logentry_template,
+        }, 
+        'exclude': ['password', 'date_joined',],
+        'allow_missing': True,
+    }
+
+    def working_set(self):
+        # I don't override the method, so that the user will get access to all
+        # users :-)
+        return super(ModelHandlerExample, self).working_set()
+
+    def filter_id(self, data):
+        ids = self.request.GET.getlist('id')
+        if ids:
+            data = User.objects.filter(id__in=ids)            
+        return data            
+
+    def order_data(self, data, order):
+        if order == 'username':
+            return data.order_by('username')
+        
+        return data
+modelhandler_proxy = Proxy(ModelHandlerExample)
+    
+
+class TestModelHandler(TestCase):
+    def setUp(self):
+        mommy.make(User, 10)
+
+        # Generate valid JWT token for User with id=1
+        headers = {
+            'typ': 'JWT', 
+            'alg': 'HS512', 
+        }
+        payload = {
+            'iss': 1,                                     # issuer: The user making the request       
+            'iat': timezone.now(),                        # issued at
+            'exp': timezone.now() +  timedelta(days=1),   # expires at
+        }            
+        self.token = jwt.encode(
+             headers=headers,
+             payload=payload, 
+             key=settings.SECRET_KEY,
+             algorithm=headers['alg'],
+         )
+
+    def test_not_authenticated(self):
+        # request is not authenticated
+        request = RequestFactory().get('')
+        response = modelhandler_proxy(request)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_singular(self):
+        # singular, authenticated request
+        request = RequestFactory().get(
+            '', 
+            HTTP_AUTHORIZATION='JWT %s' % self.token
+        )
+        kwargs = {'id': 5}
+        
+        response = modelhandler_proxy(request, **kwargs)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json; charset=utf-8')
+        
+        content = json.loads(response.content)['data']
+        self.assertIsInstance(content, dict)
+        self.assertEqual(content['id'], 5)
+
+    def test_get_plural(self):
+        # plural authenticated request
+        request = RequestFactory().get(
+            '', 
+            HTTP_AUTHORIZATION='JWT %s' % self.token
+        )
+        
+        response = modelhandler_proxy(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json; charset=utf-8')
+        
+        content = json.loads(response.content)['data']
+        self.assertIsInstance(content, list)
+        self.assertEquals(len(content), 10)
+        
+        # If ordering is the default, by id
+        user_ids = [user['id'] for user in content]
+        self.assertItemsEqual(user_ids, range(1, 11))
+
+    def test_get_plural_filter_id(self):
+        request = RequestFactory().get(
+            '?id=1&id=2&id=3', 
+            HTTP_AUTHORIZATION='JWT %s' % self.token
+        )
+        
+        response = modelhandler_proxy(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json; charset=utf-8')
+        
+        content = json.loads(response.content)['data']
+        self.assertIsInstance(content, list)
+        self.assertEquals(len(content), 3)
+
+        # If only the correct items have been returned
+        user_ids = [user['id'] for user in content]
+        self.assertItemsEqual(user_ids, [user.id for user in User.objects.filter(id__lt=4)])
+
+    def test_get_plural_order_username(self):
+        request = RequestFactory().get(
+            '?order=first_name', 
+            HTTP_AUTHORIZATION='JWT %s' % self.token
+        )
+                
+        response = modelhandler_proxy(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json; charset=utf-8')
+        
+        content = json.loads(response.content)['data']
+        self.assertIsInstance(content, list)
+        self.assertEquals(len(content), 10)
+        
+        # If ordering is correct
+        user_ids = [user['id'] for user in content]
+        self.assertItemsEqual(user_ids, [user.id for user in User.objects.order_by('username')])
+
+    def test_post_missing_field(self):
+        # missing ``username`` and ``password`` field. Should return status code 400
+        request = RequestFactory().post(
+            '/',
+            data=json.dumps({'first_name': 'name', 'last_name': 'surname'}),
+            content_type='application/json',           
+            HTTP_AUTHORIZATION='JWT %s' % self.token
+        )
+                
+        response = modelhandler_proxy(request)
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_valid(self):
+        request = RequestFactory().post(
+            '/',
+            data=json.dumps({
+                'first_name': 'name', 'last_name': 'surname',
+                'username': 'username', 'password': 'password',
+            }),
+            content_type='application/json',           
+            HTTP_AUTHORIZATION='JWT %s' % self.token
+        )
+                
+        response = modelhandler_proxy(request)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_delete_singular(self):
+        request = RequestFactory().delete(
+            '',
+            HTTP_AUTHORIZATION='JWT %s' % self.token
+        )
+        kwargs = {'id': 5}
+
+        response = modelhandler_proxy(request, **kwargs)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json; charset=utf-8')
+        
+        content = json.loads(response.content)['data']
+        self.assertIsInstance(content, dict)
+        self.assertEqual(content['id'], 5)
+
+    def test_delete_plural(self):
+        # Should not be allowed
+        request = RequestFactory().delete(
+            '',
+            HTTP_AUTHORIZATION='JWT %s' % self.token
+        )
+
+        response = modelhandler_proxy(request)
+        self.assertEqual(response.status_code, 405)
+
+    def test_put(self):
+        # Should not be allowed
+        request = RequestFactory().put(
+            '',                
+            HTTP_AUTHORIZATION='JWT %s' % self.token
+        )
+        response = modelhandler_proxy(request)
+        self.assertEqual(response.status_code, 405)
 
