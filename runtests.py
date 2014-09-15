99b5d449a4383fe864a48cc3d09cd1221949b581
stargazer <already.late@gmail.com>
2014-09-15 12:21:38 +0300

    Necessary for Django 1.7

--- runtests.py
+++ runtests.py
@@ -3,6 +3,11 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'testproject.settings'
 test_dir = os.path.dirname(__file__)
 sys.path.insert(0, test_dir)
 
+# Necessary for Django 1.7. See https://docs.djangoproject.com/en/dev/releases/1.7/#app-loading-changes
+import django
+if django.get_version().startswith('1.7'):
+    django.setup()
+
 from django.test.utils import get_runner
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
